b4cc3d477d8510a99cabbfb921ded092e6e260f2
Mark Ng <mark@markng.co.uk>
2010-04-04 22:05:01 -0700

    add first part of the API in

--- itchanged/hnewsparser/urls.py
+++ itchanged/hnewsparser/urls.py
@@ -1,6 +1,14 @@
 from django.conf.urls.defaults import *
+from piston.resource import Resource
+from piston.authentication import HttpBasicAuthentication
+from hnewsparser.handlers import StoryHandler
 
+auth = HttpBasicAuthentication(realm="ItChanged API")
+ad = { 'authentication': auth }
+
+story_resource = Resource(handler=StoryHandler, **ad)
 
 urlpatterns = patterns('',
     (r'^parse$', 'hnewsparser.views.parse', {}, "hnews-parse"),
+    url(r'^api/story/(?P<story_url>[*.]+)$', story_resource),
 )
