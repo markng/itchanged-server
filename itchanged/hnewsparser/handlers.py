583a75c2ced84d6937a99f1ef2ed44445b3786b7
Mark Ng <mark@markng.co.uk>
2010-04-08 10:12:28 -0700

    adding handlers

---
+++ itchanged/hnewsparser/handlers.py
@@ -0,0 +1,48 @@
+from piston.handler import BaseHandler
+from hnewsparser.models import Story
+from django.contrib.auth.models import User
+import base64
+import random
+import string
+
+class StoryHandler(BaseHandler):
+    """REST API for Stories"""        
+    allowed_methods = ('GET', 'PUT')
+    fields = ('url', 'comphash')
+    model = Story
+    
+    def read(self, request):
+        story, created = Story.objects.get_or_create(url=request.GET.get('story_url'))
+        story.save()
+        return story
+    
+    def update(self, request):
+        story, created = Story.objects.get_or_create(url=request.GET.get('story_url'))
+        story.comphash = request.PUT.get('comphash') # blindly trust, for the moment. FIX; send to celery queue to be processed
+        story.save()
+        # TODO : attach to users list of stories
+        return story
+        
+class UserHandler(BaseHandler):
+    """REST API for user accounts"""
+    allowed_methods = ('GET',)
+    fields = ('username', 'password')
+    model = User
+    
+    def read(self, request):
+        """return a new random username and password"""
+        newusername = base64.b64encode(str(random.randint(100000000, 999999999)))
+        while len(User.objects.filter(username=newusername)) >= 1:
+            newusername = base64.b64encode(str(random.randint(100000000, 999999999)))
+        
+        chars = string.letters + string.digits
+        newpassword = ''
+        for i in range(50):
+            newpassword = newpassword + random.choice(chars)
+        
+        user = User()
+        user.username = newusername
+        user.password = newpassword
+        user.save()
+        userdetails = { 'username' : newusername, 'password' : newpassword }
+        return userdetails
